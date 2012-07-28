9a7319868883778f5a9ece8078faaca4b46cd5d6
nikkybams <nikkybams@gmail.com>
2012-07-28 14:14:15 -0400

    php file to read messages from the dbase

---
+++ php/getMessage.php
@@ -0,0 +1,23 @@
+<?php 
+header('Content-type: application/json');
+
+ob_start();
+
+	$con = mysql_connect("localhost","test","test");
+			if (!$con)
+		{
+			die('Could not connect: ' . mysql_error());
+		}
+			$db_selected = mysql_select_db('timecapsule',$con);
+			if($db_selected)
+			{
+				$my_query = "select * from messages";
+				$result = mysql_query($my_query);
+				
+				$row = mysql_fetch_array($result,MYSQL_ASSOC);				
+				
+				echo json_encode($row);
+	}
+	
+	ob_flush()
+	?>
